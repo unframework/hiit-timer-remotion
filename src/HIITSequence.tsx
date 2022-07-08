4dc3da16a7cc19f45c59a3148e6605efedd8421b
Nick Matantsev <nick.matantsev@gmail.com>
2022-07-07 21:52:25 -0400

    Simple countdown

--- src/HIITSequence.tsx
+++ src/HIITSequence.tsx
@@ -1,11 +1,21 @@
-import { AbsoluteFill } from 'remotion';
+import { AbsoluteFill, useCurrentFrame } from 'remotion';
 
 import './HIITSequence.css';
 
 export const HIITSequence: React.FC = () => {
+  const frame = useCurrentFrame();
+  const endFrame = 60 * 24;
+  const seconds = Math.floor((endFrame - frame - 1) / 24);
+  const minutes = Math.floor(seconds / 60);
+
+  const ss = `00${seconds % 60}`.slice(-2);
+  const mm = `00${minutes % 60}`.slice(-2);
+
   return (
     <AbsoluteFill style={{ backgroundColor: 'white' }}>
-      <div className="_counter">00:00</div>
+      <div className="_counter">
+        {mm}:{ss}
+      </div>
     </AbsoluteFill>
   );
 };
